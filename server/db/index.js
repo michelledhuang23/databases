df12d3788eddc2defd4ac4ebf8113e917a7634e4
michelledhuang23 <michelledhuang@ucla.edu>
2019-05-01 16:00:09 -0700

    Create sqlConnection in index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,13 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var sqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'chat'
+});
 
 
+module.exports = {
+  sqlConnection: sqlConnection
+};
