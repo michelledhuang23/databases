1a181c83d0609fae19fe5bdff8e7d6d7a3ce434e
michelledhuang23 <michelledhuang@ucla.edu>
2019-05-01 17:13:56 -0700

    Create schema for chatterbox app

--- server/db/index.js
+++ server/db/index.js
@@ -3,9 +3,11 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+// NOTE: using student because root requires password to access
 var sqlConnection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
+  user: 'student',
+  password: 'student',
   database: 'chat'
 });
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,27 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100)
+);
 
-
+CREATE TABLE messages(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT,
+  room_id INT,
+  text VARCHAR(200),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
 
 
 /*  Execute this file from the command line by typing:
